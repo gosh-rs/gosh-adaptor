ea07fb63c0996e2f08531521f0b06641c5294b2d
Wenping Guo <ybyygu@gmail.com>
2020-01-08 11:39:36 +0800

    parse molecule from gaussian fchk; fix energy/forces unit issue

--- src/gaussian/gaussian_fchk.rs
+++ src/gaussian/gaussian_fchk.rs
@@ -1,6 +1,12 @@
 // imports
 
-use crate::parser::*;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+use gosh_core::guts::prelude::*;
+use gosh_models::ModelProperties;
 
 // collect parts
 
@@ -61,6 +67,12 @@ impl FchkDataRecords {
 
 // model
 
+// Hartree to eV
+const HARTREE: f64 = 27.211386024367243;
+const BOHR: f64 = 0.5291772105638411;
+
+use gosh_core::gchemol::Molecule;
+
 /// Parse model properties from Gaussian/fchk file.
 pub(crate) fn parse_gaussian_fchk<P: AsRef<Path>>(fchkfile: P) -> Result<ModelProperties> {
     let r = text_file_reader(fchkfile)?;
@@ -69,12 +81,14 @@ pub(crate) fn parse_gaussian_fchk<P: AsRef<Path>>(fchkfile: P) -> Result<ModelPr
     let mut mp = ModelProperties::default();
     let energy_token = "Total Energy                               R";
     let n = energy_token.len();
+    let mut symbols = vec![];
+    let mut positions = vec![];
     for (label, data) in parts {
         match &label[..n] {
             "Total Energy                               R" => {
                 let (_, e) = label.split_at(n);
                 let energy: f64 = e.trim().parse().unwrap();
-                mp.set_energy(energy);
+                mp.set_energy(energy * HARTREE);
             }
             "Dipole Moment                              R" => {
                 let d: Vec<_> = data
@@ -86,39 +100,45 @@ pub(crate) fn parse_gaussian_fchk<P: AsRef<Path>>(fchkfile: P) -> Result<ModelPr
             "Cartesian Gradient                         R" => {
                 let d: Vec<_> = data
                     .split_whitespace()
-                    .map(|x| -x.parse::<f64>().unwrap())
+                    .map(|x| HARTREE / BOHR * -x.parse::<f64>().unwrap()) // unit conversion
                     .collect();
                 let gradients: Vec<_> = d.chunks_exact(3).map(|x| [x[0], x[1], x[2]]).collect();
                 mp.set_forces(gradients);
             }
+            "Atomic numbers                             I" => {
+                symbols = data.split_whitespace().map(|s| s.to_owned()).collect();
+            }
             "Current cartesian coordinates              R" => {
-                // TODO
+                let d: Vec<_> = data
+                    .split_whitespace()
+                    .map(|x| BOHR * x.parse::<f64>().unwrap()) // unit conversion
+                    .collect();
+                positions = d.chunks_exact(3).map(|x| [x[0], x[1], x[2]]).collect();
             }
             _ => {
                 // ignore
             }
         }
     }
+    let mut mol = Molecule::new("gaussian fchk");
+    let atoms = symbols.into_iter().zip(positions.into_iter());
+    mol.add_atoms_from(atoms);
+    mp.set_molecule(mol);
 
     Ok(mp)
 }
 
 // test
 
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::BufReader;
-use std::path::Path;
-
-use gosh_core::guts::prelude::*;
-use gosh_models::ModelProperties;
-
 #[test]
 fn test_gaussian_fchk() -> Result<()> {
+    use approx::*;
+
     let fchkfile = "tests/files/gaussian/Test.FChk";
     let mp = parse_gaussian_fchk(fchkfile)?;
-    let energy = mp.get_energy();
-    assert_eq!(energy, Some(-1.177266855882589E+02));
+    let energy = mp.get_energy().expect("fchk energy");
+    assert_relative_eq!(energy, -1.177266855882589E+02 * HARTREE, epsilon = 1e-4);
+
     let dipole = mp.get_dipole();
     assert_eq!(
         dipole,
@@ -127,7 +147,17 @@ fn test_gaussian_fchk() -> Result<()> {
 
     let forces = mp.get_forces().expect("fchk forces");
     assert_eq!(forces.len(), 11);
-    assert_eq!(forces[0], [9.44446832E-03, 4.83256604E-03, 1.82167947E-02]);
+    assert_relative_eq!(
+        forces[0][0],
+        9.44446832E-03 * HARTREE / BOHR,
+        epsilon = 1e-4
+    );
+
+    let mol = mp.get_molecule().expect("fchk mol");
+    assert_eq!(mol.natoms(), 11);
+
+    let positions = mol.positions();
+    assert_relative_eq!(positions[0][0], -6.57759708E+00 * BOHR);
 
     Ok(())
 }
